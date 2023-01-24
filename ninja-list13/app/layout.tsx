26e07c5242fdb2660d2b229815d6d830f2cb2453
Ian Iversen <ianiversen1@gmail.com>
2023-01-24 16:29:17 -0500

    routing now working in ninja-list13 made with next.js 13

---
+++ ninja-list13/app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import Navbar from './comps/Navbar'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <Navbar></Navbar>
+      <body>{children}</body>
+    </html>
+  )
+}
