ced5ef18929b94dbb789c0ae82bc9b9064915404
Ian Iversen <ianiversen1@gmail.com>
2023-01-24 21:23:52 -0500

    consuming an api with getStaticProps

--- ninja-list2/pages/ninjas/index.tsx
+++ ninja-list2/pages/ninjas/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function ninjas() {
+
+
+
+export const getStaticProps = async () => {
+  
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+
+  return {
+    props: { ninjas: data}
+  }
+
+  
+}
+
+function ninjas({ ninjas }) {
+
   return (
     <div>
         <h1>All Ninjas</h1>
+        {ninjas.map(ninja => (
+          <div key={ninja.id}>
+            <a>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
     </div>
   )
 }
