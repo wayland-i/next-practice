7bc6422651d88d563191e093bdafc695fde28676
Ian Iversen <ianiversen1@gmail.com>
2023-01-24 22:04:18 -0500

    started: deploy attempt

--- ninja-list2/pages/ninjas/[id].tsx
+++ ninja-list2/pages/ninjas/[id].tsx
@@ -1,13 +1,41 @@
 import React from 'react'
 
-function details () {
+export const getStaticPaths = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
+        }
+    })
+
+    return {
+        paths: paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const data = await res.json();
+
+    return {
+        props: {ninja: data}
+    }
+}
+
+function Details ({ ninja }) {
+
   return (
     <div>
-        <h1>
-            datails page
-        </h1>
+        <h1>{ninja.name}</h1>
+        <p>{ninja.email}</p>
+        <p>{ninja.website}</p>
+        <p>{ninja.address.city}</p>
     </div>
   )
 }
 
-export default details;
+export default Details;
