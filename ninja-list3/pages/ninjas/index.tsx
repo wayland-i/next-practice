dfc863773925a9c3f9eff61e2b394cb9f5e27b72
Ian Iversen <ianiversen1@gmail.com>
2023-01-25 19:22:55 -0500

    finished: ninja-list3

--- ninja-list3/pages/ninjas/index.tsx
+++ ninja-list3/pages/ninjas/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Link from 'next/link';
 
-function index() {
+
+export const getStaticProps = async () => {
+
+	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+  
+	return {
+		props: { ninjas: data}
+	}
+}
+
+function index({ ninjas }) {
   return (
     <div>
-        <h1>Ninja List</h1>
+        {ninjas.map( ninja => (
+          <div key={ninja.id}>
+            <Link href={`/ninjas/${ninja.id}`}>
+              <h1>{ninja.name}</h1>
+            </Link>
+          </div>
+        ))}
     </div>
   )
 }
